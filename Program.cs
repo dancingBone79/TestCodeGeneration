a5238d69eb3470c3ad39806e2075102e9d2b171c
dancingBone79 <llx_79@126.com>
2024-05-26 17:04:00 +0800

    implement the logic of the querying requirement file

--- Program.cs
+++ Program.cs
@@ -22,11 +22,54 @@ namespace TestCodeGeneration
 
 		}
 
-		static List<string> QueryParameters(string functionName)
+		static string ExtractParenthesesContent(string input)
+		{
+			// 提取括号及其内容的函数
+			// 查找第一个左括号的索引
+			int leftParenthesesIndex = input.IndexOf('(');
+			if (leftParenthesesIndex == -1)
+			{
+				throw new Exception("Left parentheses not found.");
+			}
+
+			// 查找第一个右括号的索引
+			int rightParenthesesIndex = input.IndexOf(')');
+			if (rightParenthesesIndex == -1)
+			{
+				throw new Exception("Right parentheses not found.");
+			}
+
+			// 提取括号及其内容
+			string content = input.Substring(leftParenthesesIndex, rightParenthesesIndex - leftParenthesesIndex + 1);
+			return content;
+		}
+
+		static List<string> QueryParameters(string className, string functionName)
 		{
 			//查询 remodel， 取出参数的类型的列表
+			string path = @"C:\Users\p2215981\Desktop\Liu.Lixue\coroutine-program\RequirementAnalysisTests\cocome.remodel";
+			var reModelContent = File.ReadAllText(path);
+			string[] lines = reModelContent.Split("\n");
 
-			return new List<string>(new string[] { "Integer", "String", "String", "Boolean" });
+			List<string> parameterTypes = new List<string>();
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].Contains(className + "::" + functionName))
+				{
+					string contentLine = ExtractParenthesesContent(lines[i]);
+
+					var parameterEnum = contentLine.Split(",");
+					for (int j = 0; j < parameterEnum.Length; j++)
+					{
+						parameterTypes.Add(parameterEnum[j].Split(":")[1].Trim(' ', '(', ')'));
+					}
+
+					return parameterTypes;
+				}
+			}
+			//如果前面找到了，直接走上面的return，这里需要输出在remodel文件中找不到该函数的状态。
+			throw new ArgumentException($"remodel file donnot contain this function: {className + "::" + functionName}");
+
 		}
 
 		static string GetValueOfType(string type)
@@ -45,10 +88,10 @@ namespace TestCodeGeneration
 		}
 
 
-		static string GenerateFunctionCall(string functionName)
+		static string GenerateFunctionCall(string className, string functionName)
 		{
 			string code = functionName + "(";
-			var parameters = QueryParameters(functionName);
+			var parameters = QueryParameters(className, functionName);
 
 			code += string.Join(", ", parameters.Select(parameterType => GetValueOfType(parameterType)));
 
@@ -69,8 +112,9 @@ namespace TestCodeGeneration
 				foreach (var function in compositionOrder)
 				{
 					var parts = function.Split("::");
+					string className = parts[0];
 					string functionName = parts[1];
-					code += GenerateFunctionCall(functionName);
+					code += GenerateFunctionCall(className, functionName);
 				}
 				return code;
 			}
@@ -78,7 +122,6 @@ namespace TestCodeGeneration
 			{
 
 				throw new NotSupportedException($"Generating code in {targetLanguage} is out of scope of this paper.");
-				return "#include <std>";
 			}
 			else
 				throw new NotSupportedException($"Generating code in {targetLanguage} is out of scope of this paper.");
